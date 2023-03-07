0dc36ee9f866e05183cc5fc2e1c188c3dc614128
Ryan Costa <ryan.costa122@gmail.com>
2023-03-07 17:52:59 -0300

    first commit

--- src/data/questions_complete.js
+++ src/data/questions_complete.js
@@ -1,93 +1,145 @@
-const data = [
-  {
-    category: "HTML",
-    questions: [
-      {
-        question: "Qual tag cria um parágrafo?",
-        options: ["<p>", "<h1>", "<text>", "<ul>"],
-        answer: "<p>",
-        tip: "É uma tag de uma letra apenas",
-      },
-      {
-        question: "Qual atributo adiciona um link para a tag a?",
-        options: ["alt", "href", "src", "link"],
-        answer: "href",
-        tip: "Hyperlink Reference",
-      },
-      {
-        question: "As listas não ordenadas tem a tag de:",
-        options: ["<ol>", "<ul>", "<li>", "<list>"],
-        answer: "<ul>",
-      },
-      {
-        question: "Qual atributo deixa o input obrigatório?",
-        options: ["placeholder", "value", "required", "maxlength"],
-        answer: "required",
-      },
-      {
-        question: "A tag semântica indicada para rodapés é a:",
-        options: ["div", "main", "section", "footer"],
-        answer: "footer",
-      },
-    ],
-  },
-  {
-    category: "CSS",
-    questions: [
-      {
-        question: "Qual regra altera a cor de um elemento?",
-        options: ["color", "background-color", "font-size", "transition"],
-        answer: "color",
-        tip: "Cor em inglês",
-      },
-      {
-        question: "Para aumentar a fonte de um elemento utilizamos:",
-        options: ["font", "text-transform", "font-size", "hover"],
-        answer: "font-size",
-      },
-      {
-        question: "A posição que deixa um elemento fixo é a:",
-        options: ["static", "absolute", "fixed", "relative"],
-        answer: "fixed",
-      },
-    ],
-  },
-  {
-    category: "JavaScript",
-    questions: [
-      {
-        question: "O que é Vanilla JavaScript?",
-        options: [
-          "JavaScript puro",
-          "Uma biblioteca JavaScript",
-          "Um framework JavaScript",
-          "Um compilador de JavaScript",
-        ],
-        answer: "JavaScript puro",
-      },
-      {
-        question: "Com qual instrução declaramos uma constante em JavaScript?",
-        options: ["const", "let", "var", "define"],
-        answer: "const",
-      },
-      {
-        question: "Qual dos tipos de dado a seguir não existe em JavaScript?",
-        options: ["string", "number", "boolean", "float"],
-        answer: "float",
-      },
-      {
-        question: "Qual dos métodos a seguir seleciona um elemento?",
-        options: ["querySelector", "parseInt", "sort", "reduce"],
-        answer: "querySelector",
-      },
-      {
-        question:
-          "Qual destas propriedades da a quantidade de elementos de um array?",
-        options: ["qty", "length", "items", "index"],
-        answer: "length",
-      },
-    ],
-  },
-];
 
-export default data;
+  const data = [
+    {
+      category: "Política",
+      questions: [
+        {
+          question: "Qual o papel do Presidente da República?",
+          options: [
+            "a) Chefiar o Congresso Nacional",
+            "b) Chefiar o Supremo Tribunal Federal",
+            "c) Chefiar o Poder Executivo",
+            "d) Chefiar o Poder Legislativo"
+          ],
+          answer: "c) Chefiar o Poder Executivo"
+        },
+        {
+          question: "Qual a função do Congresso Nacional?",
+          options: [
+            "a) Elaborar as leis federais",
+            "b) Executar as leis federais",
+            "c) Fiscalizar o Poder Judiciário",
+            "d) Fiscalizar os Tribunais de Contas"
+          ],
+          answer: "a) Elaborar as leis federais"
+        },
+        {
+          question: "Quem é o responsável pelo julgamento de crimes comuns e de responsabilidade do Presidente da República?",
+          options: [
+            "a) O Poder Executivo",
+            "b) O Congresso Nacional",
+            "c) O Poder Judiciário",
+            "d) O Tribunal de Contas da União"
+          ],
+          answer: "c) O Poder Judiciário"
+        },
+        {
+          question: "Qual é a função do Supremo Tribunal Federal?",
+          options: [
+            "a) Elaborar as leis federais",
+            "b) Julgar ações contra decisões do Poder Executivo",
+            "c) Julgar casos de conflitos entre leis estaduais e federais",
+            "d) Fiscalizar as contas públicas"
+          ],
+          answer: "c) Julgar casos de conflitos entre leis estaduais e federais"
+        },
+        {
+          question: "Qual é a função do Tribunal de Contas da União?",
+          options: [
+            "a) Julgar ações contra decisões do Poder Executivo",
+            "b) Elaborar as leis federais",
+            "c) Fiscalizar as contas públicas",
+            "d) Julgar casos de conflitos entre leis estaduais e federais"
+          ],
+          answer: "c) Fiscalizar as contas públicas"
+        }
+      ]
+    },
+    {
+      category: "Finanças",
+      questions: [
+        {
+          question: "O que é juros compostos?",
+          options: [
+            "Juros que são calculados apenas sobre o valor principal",
+            "Juros que são calculados sobre o valor principal e os juros acumulados",
+            "Juros que são calculados com taxas variáveis",
+            "Juros que são calculados apenas com taxas fixas"
+          ],
+          answer: "Juros que são calculados sobre o valor principal e os juros acumulados"
+        },
+        {
+          question: "O que é a bolsa de valores?",
+          options: [
+            "Um mercado onde empresas vendem seus produtos",
+            "Um mercado onde são negociados títulos e valores mobiliários",
+            "Um mercado onde empresas compram ações",
+            "Um mercado onde são negociados imóveis"
+          ],
+          answer: "Um mercado onde são negociados títulos e valores mobiliários"
+        },
+        {
+          question: "O que é um fundo de investimento?",
+          options: [
+            "Um investimento de alto risco",
+            "Um investimento que garante retorno fixo",
+            "Uma empresa que administra investimentos de terceiros",
+            "Uma conta bancária de alto rendimento"
+          ],
+          answer: "Uma empresa que administra investimentos de terceiros"
+        },
+        {
+          question: "O que é um CDI?",
+          options: [
+            "Um Certificado de Depósito Bancário",
+            "Um Certificado de Depósito Interbancário",
+            "Um Contrato de Depósito Bancário",
+            "Um Contrato de Depósito Interbancário"
+          ],
+          answer: "Um Certificado de Depósito Interbancário"
+        },
+        {
+          question: "O que é um índice financeiro?",
+          options: [
+            "Uma taxa de juros fixa",
+            "Uma medida estatística utilizada para medir o desempenho do mercado financeiro",
+            "Um instrumento financeiro de alto risco",
+            "Um tipo de ação negociada na bolsa de valores"
+          ],
+          answer: "Uma medida estatística utilizada para medir o desempenho do mercado financeiro"
+        }
+      ]
+    },
+    {
+      category: "Impostos",
+      questions: [
+        {
+          question: "O que significa FGTS?",
+          options: ["Fundo de Garantia do Tempo de Serviço", "Fundo Global de Trabalho e Segurança", "Fundo de Garantia do Trabalhador Social", "Fundo Geral de Tributos e Serviços"],
+          answer: "Fundo de Garantia do Tempo de Serviço",
+        },
+        {
+          question: "Qual é a alíquota do imposto de renda para quem recebe até R$ 1.903,98 mensais?",
+          options: ["15%", "20%", "22,5%", "27,5%"],
+          answer: "Isento",
+        },
+        {
+          question: "O que é ISS?",
+          options: ["Imposto sobre Serviços de Qualquer Natureza", "Imposto sobre a Renda da Pessoa Física", "Imposto sobre Produtos Industrializados", "Imposto sobre a Propriedade de Veículos Automotores"],
+          answer: "Imposto sobre Serviços de Qualquer Natureza",
+        },
+        {
+          question: "O que é IPTU?",
+          options: ["Imposto Predial e Territorial Urbano", "Imposto sobre a Renda da Pessoa Física", "Imposto sobre Produtos Industrializados", "Imposto sobre a Propriedade de Veículos Automotores"],
+          answer: "Imposto Predial e Territorial Urbano",
+        },
+        {
+          question: "Qual é a alíquota do IOF para operações de crédito a pessoas físicas?",
+          options: ["0,38%", "1,1%", "3%", "6,38%"],
+          answer: "3%",
+        },
+      ],
+    }
+  ]
+  
+  export default data;
